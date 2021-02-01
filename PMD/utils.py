8140b7532de6515f8bdcfc4eb8549579666a9aca
steven <aeghnnsw@gmail.com>
2021-01-31 21:20:28 -0500

    fix bugs for running MD simulations

--- PMD/utils.py
+++ PMD/utils.py
@@ -40,7 +40,7 @@ def write_plumed_file(atom_ids,frequency,force,index,direction):
     f.write('c: COM ATOMS='+atoms_str+'\n')
     f.write('k: MATHEVAL ARG=t VAR=t FUNC='+k+'*'+'cos(t*'+omega+') PERIODIC=NO\n')
     f.write('P: POSITION ATOM=c\n')
-    f.write('V: MATHEVAL ARG=P.'+direction+',k VAR=x,y FUNC=x*y\n')
+    f.write('V: MATHEVAL ARG=P.'+direction+',k VAR=x,y FUNC=x*y PERIODIC=NO\n')
     f.write('BV: BIASVALUE ARG=V')
     f.close()
 
@@ -53,31 +53,34 @@ def write_plumed_files(atom_ids,frequency,force,index):
     write_plumed_file(atom_ids,frequency,force,index,'y')
     write_plumed_file(atom_ids,frequency,force,index,'z')
 
-def run_simulation(plumed_in,GPU,direction,index,cuda='0'):
+def run_simulation(plumed_in,direction,index,cuda='0'):
     '''
     run simulations for pumped MD
     '''
     input_file = write_prod_input_file(plumed_in,index,direction)
     output = 'prod'+str(index)+'_'+direction
-    os.sys('export CUDA_VISIBLE_DEVICES='+cuda)
+    cuda_command = 'export CUDA_VISIBLE_DEVICES='+cuda
+    os.system(cuda_command)
     out_file = output+'.out'
     mdcrd_file = output+'.mdcrd'
     mdvel_file = output+'.mdvel'
     run_command = 'pmemd.cuda -O -i '+input_file+' -o '+out_file+' -p mol.prmtop -c equil.rst -x '+mdcrd_file+' -v '+mdvel_file
-    os.sys(run_command)
+    print(run_command)
+    os.system(run_command)
 
 def write_prod_input_file(plumed_in,index,direction):
     '''
     write prod input files
     return input file name
     '''
-    file_name = 'prod'+str(i)+'_'+direction+'.in'
+    file_name = 'prod'+str(index)+'_'+direction+'.in'
     f = open(file_name,'w')
     f.write(file_name+'\n')
     f.write('&cntrl\n')
-    f.write('  imin=0,irest=1,ntx=5,\n  nstlim=1000000,dt=0.001,\n  ntc=2,ntf=2,\n')
-    f.write('  cut=8.0, ntb=2, ntp=1, taup=2.0\n')
+    f.write('  imin=0,irest=1,ntx=5,\n  nstlim=10000,dt=0.001,\n  ntc=2,ntf=2,\n')
+    f.write('  cut=8.0, ntb=2, ntp=1, taup=2.0,\n')
     f.write('  ntpr=5000,ntwx=50,ntwv=50,\n')
     f.write('  ntt=3,gamma_ln=2.0,temp0=3000.0,ig=-1,\n')
-    f.write('  plumed=1,plumedfile=\''+plumed_in+'\'\n/')
+    f.write('  plumed=1,plumedfile=\''+plumed_in+'\'\n/\n')
+    f.close()
     return file_name
