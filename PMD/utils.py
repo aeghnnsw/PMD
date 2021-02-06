feca7aa973742b8ca0b1f496292068ea1bf0fc3f
Shen Wang <steven_wang@Shens-iMac.local>
2021-02-06 14:02:55 -0500

    fix bugs for utils

--- PMD/utils.py
+++ PMD/utils.py
@@ -5,6 +5,7 @@ import os
 def get_atom_ids(pdb_name,res_ids):
     '''
     Takes pdb file name, and res_ids as input and returns the atoms ids
+    Request: prepare the pdb that has added H atoms
     Inputs:
         pdb_name:   string
         res_ids:    list of integers
@@ -58,43 +59,52 @@ def write_plumed_files(atom_ids,frequency,force,index):
     write_plumed_file(atom_ids,frequency,force,index,'y')
     write_plumed_file(atom_ids,frequency,force,index,'z')
 
-def run_simulation(plumed_in,direction,index,time,cuda='0'):
+def run_simulation(direction,index,time,cuda='0'):
     '''
     run simulations for pumped MD
     Inputs:
-        plumed_in:  str the name of plumed input file ex: "pump1_x.dat"
         direction:  str
         index:      int
         cuda:       str (optional define the GPU name to use)
         time:       int in ps
     '''
     input_file = write_prod_input_file(plumed_in,index,direction,time)
-    output = 'prod'+str(index)+'_'+direction
+    output = 'prod'+str(index)
+    if index>0:
+        output = output+'_'+direction
     cuda_command = 'export CUDA_VISIBLE_DEVICES='+cuda
     os.system(cuda_command)
     out_file = output+'.out'
     mdcrd_file = output+'.mdcrd'
-    mdvel_file = output+'.mdvel'
-    run_command = 'pmemd.cuda -O -i '+input_file+' -o '+out_file+' -p mol.prmtop -c equil.rst -x '+mdcrd_file+' -v '+mdvel_file
+    #mdvel_file = output+'.mdvel'
+    run_command = 'pmemd.cuda -O -i '+input_file+' -o '+out_file+' -p mol.prmtop -c equil.rst -x '+mdcrd_file
     os.system(run_command)
 
-def write_prod_input_file(plumed_in,index,direction,time):
+def write_prod_input_file(direction,index,time):
     '''
     write prod input files
     Inputs:
         time:   int in ps
+        index:  if index=0 it is unbiased simulation
     return input file name
     '''
-    nsteps = str(int(1000000*time))
-    file_name = 'prod'+str(index)+'_'+direction+'.in'
+    nsteps = str(int(1000*time))
+    file_name = 'prod'+str(index)
+    if index>0:
+        file_name = file_name+'_'+direction
+    file_name = file_name+'.in'
     f = open(file_name,'w')
     f.write(file_name+'\n')
     f.write('&cntrl\n')
     f.write('  imin=0,irest=1,ntx=5,\n  nstlim='+nsteps+',dt=0.001,\n  ntc=2,ntf=2,\n')
     f.write('  cut=8.0, ntb=2, ntp=1, taup=2.0,\n')
-    f.write('  ntpr=5000,ntwx=50,ntwv=50,\n')
-    f.write('  ntt=3,gamma_ln=2.0,temp0=3000.0,ig=-1,\n')
-    f.write('  plumed=1,plumedfile=\''+plumed_in+'\'\n/\n')
+    f.write('  ntpr=5000,ntwx=10,\n')
+    f.write('  ntt=3,gamma_ln=2.0,temp0=300.0,ig=-1')
+    if index>0:
+        plumed_in = 'pump'+str(index)+'_'+direction+'.dat'
+        f.write(',\n  plumed=1,plumedfile=\''+plumed_in+'\'\n/\n')
+    else:
+        f.write('\n')
     f.close()
     return file_name
 
@@ -103,23 +113,33 @@ def write_cpptraj_input_file(index,direction):
     write cpptraj inpute file for extract waters and Na+ and CL-
     return the file name
     '''
-    file_name='ctj'+str(index)+'_'+direction+'.in'
+    file_name='ctj'+str(index)
+    trajin = 'trajin prod'+str(index)
+    trajout = 'trajout prod'+str(index)
+    if index>0:
+        file_name = file_name+'_'+direction
+        trajin = trajin+'_'+direction
+        trajout = trajout+'_'+direction
+    file_name=file_name+'.in'
+    trajin = trajin+'.mdcrd'
+    trajout = trajout+'_nw.xtc'
     f = open(file_name,'w')
     f.write('parm mol.prmtop\n')
-    f.write('trajin prod'+str(index)+'_'+direction+'.mdcrd\n')
+    f.write(trajin+'\n')
     f.write('strip :WAT\n')
     f.write('strip :Na+\n')
     f.write('strip :Cl-\n')
-    f.write('trajout prod'+str(index)+'_'+direction+'_nw.xtc\n')
+    f.write(trajout+'\n')
     f.close()
     return file_name 
 
-def run_cpptraj(ctj_in):
+def run_cpptraj(index,direction):
     '''
     run cpptraj to remove waters and ions of a trajectory and save the file
     Inputs:
         ctj_in: str name of cpptraj input file
-    ''' 
+    '''
+    ctj_in = write_cpptraj_input_file(index,direction)
     os.system('cpptraj -i '+ctj_in)
     return None
 
@@ -140,4 +160,11 @@ def strip_topology_wat():
         print('No mol.prmtop file, maybe in the wrong directory.')
     return None
 
-    
+def check_cuda_error(index,direction,time):
+    '''
+        Use plumed will sometimes cause cuda memory error,
+        When this happens, we want to rerun the simulation.
+    '''
+    top_file = 'mol.prmtop'
+    traj_file = 'prod'+str(index)+'_'+direction+'.mdcrd'
+
