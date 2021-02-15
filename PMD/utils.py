08a1d1df98197b52296f164573dcb21b7dc57ace
Shen Wang <steven_wang@Shens-iMac.local>
2021-02-14 21:42:43 -0500

    fix bugs

--- PMD/utils.py
+++ PMD/utils.py
@@ -1,18 +1,18 @@
 import numpy as np
 import mdtraj as md
 
-def get_atom_ids(pdb_name,res_ids,calpha=True):
+def get_atom_ids(pdb_file,res_ids,calpha=True):
     '''
     Takes pdb file name, and res_ids as input and returns the atoms ids
     Request: prepare the pdb that has added H atoms
     Inputs:
-        pdb_name:   string
+        pdb_file:   string
         res_ids:    list of integers
         calpha:     bool, default to be True, when true, only select calpha atoms
     Return:
         atom_ids:   list of integers
     '''
-    pdb = md.load_pdb(pdb_name)
+    pdb = md.load_pdb(pdb_file)
     top = pdb.topology
     atom_ids = list()
     if isinstance(res_ids,int):
@@ -28,3 +28,8 @@ def get_atom_ids(pdb_name,res_ids,calpha=True):
                 atom_ids.extend(top.select('backbone and resid '+str(res_id-1)))
     return atom_ids
 
+def get_res_num(pdb_file):
+    pdb = md.load_pdb(pdb_file)
+    return pdb.n_residues
+
+    
